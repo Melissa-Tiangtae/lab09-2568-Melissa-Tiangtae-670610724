11f40eed28e96a365939896840ce75d6b0d78b6b
Melissa Tiangtae <melissa.tiangtae@gmail.com>
2025-08-06 14:37:26 +0700

    completed

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -16,6 +16,7 @@ export default function TaskCard({
   toggleDoneTaskFunc,
 }: props) {
   // callback function when delete button is clicked
+  
   const deleteBtnOnClick = () => {
     deleteTaskFunc(id);
   };
--- src/pages/HelloMyNamePage.tsx
+++ src/pages/HelloMyNamePage.tsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 export default function HelloMyNamePage() {
+
+  const [name,setName] = useState("");
+  const[result,setResult] = useState("");
+  const [isGreetOk, setGreetOk] = useState(false)
+  const greetBtnOnClick = () => {
+
+    if(name == ""){
+      setResult("Please insert your name");
+      setGreetOk(false);
+    }else{
+    setResult(` Hello ${name}`);
+    setGreetOk(true) 
+    }
+  }
+  const nameInput =(event : any) =>{ 
+    setName(event.target.value)
+  }
+
+
   return (
     <div className="container text-center">
       <h3>Hello My Name</h3>
@@ -6,10 +27,12 @@ export default function HelloMyNamePage() {
         className="form-control d-inline m-1"
         placeholder="Insert your name here!"
         style={{ width: "300px" }}
+        onChange={nameInput}
+        value={name}
       />
-      <button className="btn btn-primary">Greet Me</button>
+      <button className="btn btn-primary"  onClick={greetBtnOnClick}>Greet Me</button>
       {/* Result Text */}
-      <p></p>
+      <p className={isGreetOk? " " : "text-danger"} >{result}</p>
     </div>
   );
 }
